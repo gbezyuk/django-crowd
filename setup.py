f4314aa30cb8e98cdd49a46d141e03d657016b91
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-08-27 23:54:13 +0400

    modified setup.py (package renamed)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='Django-Crowd',
+    name='django-crowd',
     version='0.4dev',
     packages=['crowd',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
