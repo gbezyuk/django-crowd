dc6d7b4d4e4f8d543b02e8e8ed3f8d4bde81c666
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-08-28 00:50:11 +0400

    TODO.txt + v0.41 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-crowd',
-    version='0.4dev',
+    version='0.41dev',
     packages=['crowd',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
