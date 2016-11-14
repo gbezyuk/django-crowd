a4323453e96db03813dcb971d73433ebb00b7bfc
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-11-15 01:15:33 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from distutils.core import setup
 
 setup(
     name='django-crowd',
-    version='0.41dev',
+    version='0.42dev',
     packages=['crowd',],
     install_requires=['httplib2'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
+    author="Konstantin Volkov",
+    author_email="konstantin-j-volkov@yandex.ru",
+    maintainer="Grigoriy Beziuk",
+    maintainer_email="gbezyuk@gmail.com"
 )
